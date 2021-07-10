1ee95fbd188f01876b27d34ea1ab20a87b0ecde9
yongencui <549852417@qq.com>
2021-07-10 11:15:32 +0800

    test

---
+++ Python/hello_world.py
@@ -0,0 +1,14 @@
+import re 
+# f1 = 1
+# f2 = 1
+# for i in range(1,22):7
+#     print ('%12ld %12ld' % (f1,f2))
+#     if (i % 3) == 0:
+#         print ('')
+#     f1 = f1 + f2
+#     f2 = f1 + f2
+
+import re
+print(re.search('www', 'www.runoob.com').group(0))
+print(re.search('com', 'www.runoob.com').span())
+
